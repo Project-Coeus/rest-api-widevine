1dc15d2f0e80dc3bdb20a956871c450bff3e071f
rala-l <81008522+rala-l@users.noreply.github.com>
2023-09-01 12:29:32 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import requests
 import jsonschema
 import random
+import os
 from flask import Flask, jsonify, request, json
 from jsonschema import validate
 from pywidevine.cdm import Cdm
@@ -40,7 +41,7 @@ def get_decryption_keys(_pssh, license_url, _headers, wvd_id):
     try:
         wvd_data = next(wvd for wvd in wvd_provisions if wvd["id"] == wvd_id)
         pssh = PSSH(_pssh)
-        device = Device.load("./../wvd/" + wvd_data['file'])
+        device = Device.load(os.path.normpath(os.getcwd() + os.sep + os.pardir) + "/wvd/" + wvd_data['file'])
         cdm = Cdm.from_device(device)
         session_id = cdm.open()
         challenge = cdm.get_license_challenge(session_id, pssh)
